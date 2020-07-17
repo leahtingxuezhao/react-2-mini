1e1ca2d294cedbf5b0221b2c20ac652b1d27cbf4
Leah Zhao <leahtingxuezhao@gmail.com>
2020-07-17 13:58:19 -0600

    finished

--- src/components/TextContainer.js
+++ src/components/TextContainer.js
@@ -1,24 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class TextContainer extends Component {
   constructor() {
     super();
     this.state = {
-      text: '',
+      text: "",
     };
   }
 
-  updateText(e) {
+  updateText = (e) => {
     this.setState({
       text: e.target.value,
     });
-  }
+  };
 
   render() {
     return (
       <div className="textContainer">
         <textarea
-          // style={  }
+          style={{
+            color: this.props.fontColor,
+            fontFamily: this.props.fontFamily,
+            fontSize: this.props.fontSize,
+          }}
           onChange={this.updateText}
           value={this.state.text}
           placeholder="Start typing your thoughts here!"
